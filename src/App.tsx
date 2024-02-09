b97a57d4099971cb7f23cdbf732bed4b9257c3bb
Julia <143531581+JulCD@users.noreply.github.com>
2024-02-09 14:59:23 -0600

    Getting used to using tailwind and daisyui
    Messing with card components and other tailwind behavior for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App p-3">
       <button className="btn">Hello daisyUI</button>
+      <div className="card bg-base-100 shadow-lg">
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
