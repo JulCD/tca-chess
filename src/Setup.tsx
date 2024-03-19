26df65a1f3c62f81f3b344c1561ebf2f2b712d4a
Julia <143531581+JulCD@users.noreply.github.com>
2024-03-19 13:30:46 -0500

    Local state for checkboxes now working
    Kinda confusing...

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { TransformStreamDefaultController } from "stream/web";
 
 interface SetupProps {
     setTitle: (t: string) => void;
@@ -30,6 +31,7 @@ export const Setup: FC<SetupProps> = ({ setTitle, previousPlayers }) => {
             <button
                 className="btn btn-lg btn-primary"
                 onClick={() => nav('/play')}
+                disabled={availablePlayers.filter(x => x.checked).length < 2}
             >
                 Start the Game
             </button>
@@ -46,9 +48,27 @@ export const Setup: FC<SetupProps> = ({ setTitle, previousPlayers }) => {
                                 className="form-control"
                                 key={x.name}
                             >
-                                <label className="cursor-pointer flex">
-                                    <input type="checkbox" className="checkbox checkbox-success" />
-                                    <span className="label-text ml-3">{x.name}</span>
+                                <label 
+                                    className="cursor-pointer flex"
+                                >
+                                    <input 
+                                        type="checkbox" 
+                                        className="checkbox checkbox-success"
+                                        checked={x.checked}
+                                        onChange={() => setAvailablePlayers([
+                                            ...availablePlayers.map(y => ({
+                                                name: y.name
+                                                , checked: y.name === x.name
+                                                    ? !y.checked
+                                                    : y.checked
+                                            }))
+                                        ])} 
+                                    />
+                                    <span 
+                                        className="label-text ml-3"
+                                    >
+                                        {x.name}
+                                    </span>
                                 </label>
                             </div>
 
